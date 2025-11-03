c5f53d38723a10bbf80e47b8b9e969a537935261
nezuchi <nezuchi@localhost>
2025-11-03 23:23:12 +0900

    2.3　Java導入

---
+++ sample/src/sample/Sample.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//氏名
+		String name = "小濵　実峰";
+		//年齢
+		int age = 29;
+		//趣味
+		String hobby = "絵を描くこと・歌うこと・配信";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
