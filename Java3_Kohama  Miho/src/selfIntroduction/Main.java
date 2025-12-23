b3e60bf9b3450561e994b929b9b48c10481a9566
nezuchi <nezuchi@localhost>
2025-12-23 11:53:54 +0900

    JavaNew2.23_自己紹介プログラムを作成

---
+++ Java3_Kohama  Miho/src/selfIntroduction/Main.java
@@ -0,0 +1,16 @@
+package selfIntroduction;
+
+class Main {
+	 public static void main(String[] args) {
+		 Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		 
+		 // System.out.println(person1.name);
+		 // System.out.println(person1.age);
+		 // System.out.println(person1.height);
+		 
+		 person1.print();
+		 
+		 System.out.println("合計" + Person.count + "人です");
+		 
+	}
+}
